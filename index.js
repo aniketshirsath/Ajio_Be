416329b623dadc4943c7837ac8abb7e5d5026d05
aniketshirsath <aniketshirsath1411@gmail.com>
2025-06-17 13:47:40 +0530

    ok

--- index.js
+++ index.js
@@ -8,17 +8,29 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Read JSON data
 const rawData = fs.readFileSync("db.json");
 const productData = JSON.parse(rawData);
 
+// Default route
 app.get("/", (req, res) => {
   res.send("Ajio Backend is working!");
 });
 
-app.get("/men", (req, res) => {
-  res.send(productData.men); // Use "products", not "product"
+// Routes
+app.get("/cart", (req, res) => {
+  res.send(productData.cart);
 });
 
+app.get("/men", (req, res) => {
+  res.send(productData.men);
+});
+
+app.get("/men-shirts", (req, res) => {
+  res.send(productData["men-shirts"]);
+});
+
+// Server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

db49fe16c89e55882baa7b2c4fc5e0ccf7d4df42
aniketshirsath <aniketshirsath1411@gmail.com>
2025-06-17 13:55:36 +0530

    ok

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-const express = require("express");
-const fs = require("fs");
-const cors = require("cors");
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3002; // you can use any port number here; i chose to use 3001
 
-const app = express();
-const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
 
-app.use(cors());
-app.use(express.json());
+server.listen(port);
 
-// Read JSON data
-const rawData = fs.readFileSync("db.json");
-const productData = JSON.parse(rawData);
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("Ajio Backend is working!");
-});
-
-// Routes
-app.get("/cart", (req, res) => {
-  res.send(productData.cart);
-});
-
-app.get("/men", (req, res) => {
-  res.send(productData.men);
-});
-
-app.get("/men-shirts", (req, res) => {
-  res.send(productData["men-shirts"]);
-});
-
-// Server
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// this json-server
